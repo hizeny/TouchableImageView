04f080cf1765ef20252d121ba33f2575852c4bf6
yosuke.hizen <yosuke.hizen@gmail.com>
2013-09-25 20:55:07 +0900

    add TouchableImageView.

--- TouchableImageView/src/com/albahn/touchableimageview/TouchableImageView.java
+++ TouchableImageView/src/com/albahn/touchableimageview/TouchableImageView.java
@@ -1,10 +1,16 @@
 package com.albahn.touchableimageview;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.graphics.ColorFilter;
+import android.graphics.LightingColorFilter;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.widget.ImageView;
 
 public class TouchableImageView extends ImageView {
+
+	private ColorFilter filter = new LightingColorFilter(Color.GRAY, 0);
 
 	public TouchableImageView(Context context, AttributeSet attrs, int defStyle) {
 		super(context, attrs, defStyle);
@@ -16,6 +22,30 @@ public class TouchableImageView extends ImageView {
 
 	public TouchableImageView(Context context) {
 		super(context);
+	}
+	
+	public void setOverlayColorFilter(ColorFilter filter) {
+		this.filter = filter;
+	}
+	
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		
+		switch (event.getAction()) {
+		case MotionEvent.ACTION_DOWN:
+			
+			if (this.isClickable()) {
+				this.setColorFilter(filter);
+			}
+			break;
+
+		case MotionEvent.ACTION_UP:
+		case MotionEvent.ACTION_CANCEL:
+			this.clearColorFilter(); 
+			break;
+		}
+		
+		return super.onTouchEvent(event);
 	}
 
 }
