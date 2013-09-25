466811b9316d5dd10c03f6ba9741b8fa16fd123d
yosuke.hizen <yosuke.hizen@gmail.com>
2013-09-25 20:48:36 +0900

    first commit.

---
+++ TouchableImageView/src/com/albahn/touchableimageview/TouchableImageView.java
@@ -0,0 +1,21 @@
+package com.albahn.touchableimageview;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+public class TouchableImageView extends ImageView {
+
+	public TouchableImageView(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+	}
+
+	public TouchableImageView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+	}
+
+	public TouchableImageView(Context context) {
+		super(context);
+	}
+
+}
